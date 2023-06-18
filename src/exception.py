c5a308fdabfd857b8ba454a823f6c589f2d6c1c7
sudhirkumardubey <dubey.sudhirpp@gmail.com>
2023-06-18 02:42:01 +0200

    Data processing

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_details(error, error_detail:sys):
@@ -20,6 +20,13 @@ class CustomException(Exception):
           return self.error_message
 
      
+if __name__ == "__main__":
+     try :
+          a = 1/0
+     except Exception as e:
+          logging.info("Devivde by sero")
+          raise CustomException(e, sys)
+
     
     
 
