0904f10c8ccade74ef65d8253f6f3679720818d7
sudhirkumardubey <dubey.sudhirpp@gmail.com>
2023-06-18 17:54:02 +0200

    Hyperparameter Tunining

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,7 +50,49 @@ class ModelTrainer:
                     "Catboosting classifier": CatBoostRegressor(),
                     "Adaboost classfier": AdaBoostRegressor()
                 }
-            model_report: dict=evaluate_model(X_train=X_train, y_train = y_train, X_test=X_test, y_test =y_test, models=models)
+            
+            # Hyper parameter tuning : T7
+            params={
+                "Random forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Gradient boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbours classifier": {},
+                "XGB classifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Catboosting classifier":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboost classfier":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
+            model_report: dict=evaluate_model(X_train=X_train, y_train = y_train, X_test=X_test, y_test =y_test, models=models, params = params)
 
             # To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
